dfc5e6d8bf1a4625ddfe3a0365fd8617397a9564
prasad <gunisettyvprasad2gmail.com>
2022-08-08 22:37:10 +0530

    this is modified

--- testCases/test_Page.py
+++ testCases/test_Page.py
@@ -1,1 +1,8 @@
-
+import pytest
+from selenium import webdriver
+from pageobjects.LoginPage import LoginPage
+from pageobjects.searchCustomerPage import searchCustomer
+from pageobjects.addCustomerPage import AddCustomer
+from utilities.readProperties import Readconfig
+from utilities.customlogger import LogGen
+import time
